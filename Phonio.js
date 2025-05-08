fbe6143a87be77d9accb12db02820db92975abc7
husali726 <husali726@edu.linkoping.se>
2025-05-08 12:39:29 +0000

    2025

--- Phonio.js
+++ Phonio.js
@@ -2,20 +2,20 @@ const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
-const images = document.querySelectorAll('img')
-images.forEach(image =>{
+const images = document.querySelectorAll('.phones img')
+images.forEach(image => {
     image.addEventListener('click', e => {
         lightbox.classList.add('active')
         const img = document.createElement('img')
         img.src = image.src
-        while(lightbox.firstChild){
+        while (lightbox.firstChild) {
             lightbox.removeChild(lightbox.firstChild)
         }
         lightbox.appendChild(img)
     })
 })
+
 lightbox.addEventListener('click', e => {
     if (e.target !== e.currentTarget) return
     lightbox.classList.remove('active')
 })
-
